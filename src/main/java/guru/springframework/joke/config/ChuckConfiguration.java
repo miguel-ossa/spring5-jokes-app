a02e09efc9222a539d9f093cdb47ae65563178a6
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2020-01-25 16:54:27 +0100

    Java Configuration

---
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package guru.springframework.joke.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by mossa on 25/01/2020.
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
